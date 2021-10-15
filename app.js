16f87b10c60c1c45f28abbb17b036b3591da64d9
Divij <divij.shrivastava@gmail.com>
2021-10-15 18:14:08 +0530

    Added middleware

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ const express = require('express')
 
 const app = express();
 
-app.get('/', (req, res, next)=> {
-    res.send('<h1> Hello World! </h1>');
-})
+function middleware1(req, res, next){
+    console.log("I am middleware1.");
+    next();
+    //res.send('<h2> hello.</h2>');
+}
+
+function standardExpressCallback(requestObject, responseObject, nextMiddleWare){
+    console.log('I am standardExpressCallback.');
+    responseObject.send('<h1> Hello World! </h1>');
+}
+
+app.get('/', middleware1, standardExpressCallback);
 
 app.listen(3000);
 
