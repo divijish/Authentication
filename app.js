8431a934fc50e10d0ee05106bad155f0d833f845
Divij <divij.shrivastava@gmail.com>
2021-10-15 20:53:04 +0530

    Added property to request object

--- app.js
+++ app.js
@@ -2,25 +2,35 @@ const express = require('express')
 
 const app = express();
 
-function middleware1(req, res, next){
-    console.log("I am middleware1.");
-    const errObj = new Error('I am an error');
 
-    next (errObj);
+app.use(middleware1);
+app.use(middleware2);
+
+function middleware1(req, res, next){
+    console.log("I am middleware 1.");
+    req.customProp = 100;
+    next ();
+}
+
+function middleware2(req, res, next){
+    console.log("I am middleware 2.");
+    console.log(`Customer Property: ${req.customProp}`);
+    req.customProp = 200;
+    next();
 }
 
 function errorHandler(err, req, res, next){
     if(err){
-        res.send('<h1> There was an error, please try again</h1>');
+        res.json({err:err});
     }
 }
 
 function standardExpressCallback(requestObject, responseObject, nextMiddleWare){
     console.log('I am standardExpressCallback.');
-    responseObject.send('<h1> Hello World! </h1>');
+    responseObject.send(`<h1> Hello World! ${requestObject.customProp}</h1>`);
 }
 
-app.get('/', middleware1, standardExpressCallback);
+app.get('/',  standardExpressCallback);
 
 app.use(errorHandler);
 
